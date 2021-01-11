263539ae6f9bee496200b73993c83ff2f8460588
Hugo Batista <hugojvb20@gmail.com>
2021-01-11 23:27:08 +0000

    Deleted interfaces folder

--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -1,16 +1,17 @@
-import * as React from 'react'
-
-import { User } from '../interfaces'
+import * as React from "react";
 
 type ListDetailProps = {
-  item: User
-}
+	item: {
+		id: number;
+		name: string;
+	};
+};
 
 const ListDetail = ({ item: user }: ListDetailProps) => (
-  <div>
-    <h1>Detail for {user.name}</h1>
-    <p>ID: {user.id}</p>
-  </div>
-)
+	<div>
+		<h1>Detail for {user.name}</h1>
+		<p>ID: {user.id}</p>
+	</div>
+);
 
-export default ListDetail
+export default ListDetail;
